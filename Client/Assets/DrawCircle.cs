c5639068fb78eb763bdab8fdbc8e60015316544d
lizhiwei <18252116593@163.com>
2019-09-16 11:41:27 +0800

    c3

---
+++ Client/Assets/DrawCircle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircle : MonoBehaviour
+{
+    public Transform m_Transform;
+    public float m_Raius = 2;
+    public float m_Theta = 0.1f;
+    public Color m_color = Color.green;
+
+    private void OnDrawGizmos()
+    {
+        if (m_Transform == null) return;
+        if (m_Theta < 0.0001f) m_Theta = 0.0001f;
+
+        Matrix4x4 de = Gizmos.matrix;
+        Gizmos.matrix = m_Transform.localToWorldMatrix;
+
+        Color color = Gizmos.color;
+        Gizmos.color = m_color;
+
+        Vector3 beginPoint = Vector3.zero;
+        Vector3 firstPoint = Vector3.zero;
+
+        for (float i = 0; i < 2 * Mathf.PI; i += m_Theta)
+        {
+            float x = m_Raius * Mathf.Cos(i);
+            float z = m_Raius * Mathf.Sin(i);
+
+            Vector3 endPoint = new Vector3(x, 0, z);
+            if (i == 0)
+            {
+                firstPoint = endPoint;
+            }
+            else
+            {
+                Gizmos.DrawLine(beginPoint, endPoint);
+            }
+            beginPoint = endPoint;
+        }
+
+        Gizmos.DrawLine(firstPoint, beginPoint);
+        Gizmos.color = color;
+        Gizmos.matrix = de;
+
+    }
+}
+
---
+++ Client/Assets/particleplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleplay : MonoBehaviour
+
+{
+    private GameObject glow;
+    //private GameObject xuying;
+    private GameObject zhangangshan;
+    private GameObject yan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //glow = GameObject.Find("glow");
+        glow = transform.GetChild(1).gameObject;
+        //xuying = transform.GetChild(2).gameObject;
+        zhangangshan = transform.GetChild(3).gameObject;
+        yan = transform.GetChild(4).gameObject;
+        glow.GetComponent<ParticleSystem>().Stop();
+        //xuying.GetComponent<ParticleSystem>().Stop();
+        zhangangshan.GetComponent<ParticleSystem>().Stop();
+        yan.GetComponent<ParticleSystem>().Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void tiaozhantexiao()
+    {
+        glow.GetComponent<ParticleSystem>().Play();
+
+    }
+    public void shanbi()
+    {
+        //xuying.GetComponent<ParticleSystem>().Play();
+    }
+    public void pugongtexiao()
+    {
+        zhangangshan.GetComponent<ParticleSystem>().Play();
+    }
+    public void blink()
+    {
+        yan.GetComponent<ParticleSystem>().Play();
+    }
+}
+
